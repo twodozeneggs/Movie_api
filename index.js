f76e5fc1bb69871172a1f93012756b6386825cf6
twodozeneggs <simonjohnloik@gmail.com>
2022-04-26 18:00:11 -0500

    removed authentication

--- index.js
+++ index.js
@@ -46,20 +46,16 @@ app.get("/test", function (req, res, next) {
   res.render("test"); // This should have a view
 });
 
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movie) => {
-        res.status(201).json(movie);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", function (req, res) {
+  Movies.find()
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+});
 
 app.get(
   "/users",
